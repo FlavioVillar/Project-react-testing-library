01e985fdeb4d2b633dc0a75df94348804d838093
FlavioVillar <flavio_villar@hotmail.com>
2022-03-22 17:01:04 -0300

    requisito 02 ok - Teste o componente <About.js />

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,19 @@
-test('test', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../services/renderWithRouter';
+import { About } from '../components';
+
+describe.only('Testes do componente About.js ', () => {
+  it('Verifica se a página contém as informações sobre a Pokédex', () => {
+    renderWithRouter(<About />);
+
+    const headingPokedex = screen.getByRole(
+      'heading', { name: 'About Pokédex', level: 2 },
+    );
+    expect(headingPokedex).toBeInTheDocument();
+
+    const image = screen.getByRole('img', { name: 'Pokédex' });
+    expect(image.src).toContain('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+    // visto em https://stackoverflow.com/questions/60509527/jestreact-native-testing-library-how-to-test-an-image-src
+  });
+});
