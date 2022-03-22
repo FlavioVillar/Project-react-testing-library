fe28b5ecf92a23026ceb9db2b89f4bc4a2721205
FlavioVillar <flavio_villar@hotmail.com>
2022-03-22 17:34:57 -0300

    requisito 03 ok - Teste o componente <FavoritePokemons.js />

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -3,7 +3,7 @@ import { screen } from '@testing-library/react';
 import renderWithRouter from './helpers/renderWithRouter';
 import { About } from '../components';
 
-describe.only('Testes do componente About.js ', () => {
+describe('Testes do componente About.js ', () => {
   it('Verifica se a página contém as informações sobre a Pokédex', () => {
     renderWithRouter(<About />);
 
@@ -11,6 +11,10 @@ describe.only('Testes do componente About.js ', () => {
       'heading', { name: 'About Pokédex', level: 2 },
     );
     expect(headingPokedex).toBeInTheDocument();
+  });
+
+  it('Verifica se a página contém imagem específica de uma Pokédex:', () => {
+    renderWithRouter(<About />);
 
     const image = screen.getByRole('img', { name: 'Pokédex' });
     expect(image.src).toContain('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { FavoritePokemons } from '../components';
+import renderWithRouter from './helpers/renderWithRouter';
+
+describe('Testes do componente FavoritePokemons.js ', () => {
+  it('Verifica se os Links estão corretos', () => {
+    const { history } = renderWithRouter(<FavoritePokemons />);
+
+    const notFoundPokemon = screen.getByText(/No favorite pokemon found/i);
+    expect(notFoundPokemon).toBeInTheDocument();
+  });
+});
