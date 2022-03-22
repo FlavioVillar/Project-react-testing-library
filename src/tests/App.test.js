a63b8f9741a776d1ba0d0a553a6581581d584922
FlavioVillar <flavio_villar@hotmail.com>
2022-03-22 15:28:45 -0300

    requisito 01 ok - Teste o componente <App.js />

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../services/renderWithRouter';
+
+describe('Testes do componente App.js ', () => {
+  it('Verifica se os Links estão corretos', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const linkHome = screen.getByRole('link', { name: 'Home' });
+    userEvent.click(linkHome);
+    expect(history.location.pathname).toEqual('/');
+
+    const linkAbout = screen.getByRole('link', { name: 'About' });
+    userEvent.click(linkAbout);
+    expect(history.location.pathname).toEqual('/about');
+
+    const linkFavoritePokemons = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    userEvent.click(linkFavoritePokemons);
+    expect(history.location.pathname).toEqual('/favorites');
+  });
+});
